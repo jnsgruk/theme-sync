37c4bc28638d44a0a1b7662e5c4acf676dc06be3
Jon Seager <jon@sgrs.uk>
2026-01-16 17:28:23 +0000

    build: use `flake-parts` and enable cross-compilation to `aarch64`

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
@@ -11,79 +13,71 @@
       self,
       nixpkgs,
       rust-overlay,
-    }:
-    let
-      supportedSystems = [
+      flake-parts,
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
         "x86_64-linux"
         "aarch64-linux"
       ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-      pkgsForSystem =
-        system:
-        (import nixpkgs {
-          inherit system;
+      perSystem =
+        { config, system, ... }:
+        let
           overlays = [ (import rust-overlay) ];
-        });
-    in
-    {
-      packages = forAllSystems (
-        system:
-        let
-          inherit (pkgsForSystem system)
-            lib
-            rustPlatform
-            ;
 
-          cargoToml = lib.trivial.importTOML ./Cargo.toml;
-          version = cargoToml.package.version;
-        in
-        rec {
-          default = theme-sync;
+          pkgs = import nixpkgs { inherit system overlays; };
 
-          theme-sync = rustPlatform.buildRustPackage {
-            pname = "theme-sync";
-            version = version;
-            src = lib.cleanSource ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-
-            meta = {
-              description = "Reconfigure applications to match system theme";
-              homepage = "https://github.com/jnsgruk/theme-sync";
-              license = lib.licenses.asl20;
-              mainProgram = "theme-sync";
-              platforms = lib.platforms.unix;
-              maintainers = with lib.maintainers; [ jnsgruk ];
-            };
-          };
-        }
-      );
-
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = pkgsForSystem system;
           rust = pkgs.rust-bin.stable.latest.default.override {
             extensions = [ "rust-src" ];
           };
+
+          buildThemeSync = (
+            pkgs:
+            let
+              inherit (pkgs) lib rustPlatform;
+            in
+            rustPlatform.buildRustPackage {
+              pname = "theme-sync";
+              version = (lib.trivial.importTOML ./Cargo.toml).package.version;
+              src = lib.cleanSource ./.;
+              cargoLock.lockFile = ./Cargo.lock;
+              meta = {
+                description = "Reconfigure applications to match system theme";
+                homepage = "https://github.com/jnsgruk/theme-sync";
+                license = lib.licenses.asl20;
+                mainProgram = "theme-sync";
+                platforms = lib.platforms.unix;
+                maintainers = with lib.maintainers; [ jnsgruk ];
+              };
+            }
+          );
         in
         {
-          default = pkgs.mkShell {
-            name = "theme-sync";
-
-            NIX_CONFIG = "experimental-features = nix-command flakes";
-            RUST_SRC_PATH = "${rust}/lib/rustlib/src/rust/library";
-
-            buildInputs = [
-              rust
-            ]
-            ++ (with pkgs; [
-              nil
-              nixfmt
-              rustup
-            ]);
+          packages = {
+            default = self.packages.${system}.theme-sync;
+            theme-sync = buildThemeSync pkgs;
+            theme-sync-cross-aarch64 = buildThemeSync pkgs.pkgsCross.aarch64-multiplatform;
           };
-        }
-      );
+
+          devShells = {
+            default = pkgs.mkShell {
+              name = "theme-sync";
+
+              NIX_CONFIG = "experimental-features = nix-command flakes";
+              RUST_SRC_PATH = "${rust}/lib/rustlib/src/rust/library";
+
+              buildInputs = [
+                rust
+              ]
+              ++ (with pkgs; [
+                nil
+                nixfmt
+                rustup
+              ]);
+            };
+
+          };
+        };
     };
 }
